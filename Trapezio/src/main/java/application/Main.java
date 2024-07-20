b7bd77b53e0caf99b450909bf60ce86b737edc2f
Gabriel Rodrigues Rolim <100931450+rolim265@users.noreply.github.com>
2024-07-20 17:41:44 -0300

    Add files via upload

---
+++ Trapezio/src/main/java/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import entites.Trapezio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Trapezio trap1 = new Trapezio();
+
+        trap1.baseMaior = 2.5;
+    }
+}
---
+++ Trapezio/src/main/java/entites/Trapezio.java
@@ -0,0 +1,14 @@
+package entites;
+
+public class Trapezio {
+         public double baseMaior, baseMenor, altura;
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
